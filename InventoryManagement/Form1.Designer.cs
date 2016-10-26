43cb849626e324d55a713e9e183086daf08629ec
ol3120ao <ol3120ao@T301022-1625311>
2016-10-25 21:06:31 -0500

    Add project files.

---
+++ InventoryManagement/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace InventoryManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdoFood = new System.Windows.Forms.RadioButton();
+            this.rdoWeapons = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdoFood
+            // 
+            this.rdoFood.AutoSize = true;
+            this.rdoFood.Location = new System.Drawing.Point(12, 19);
+            this.rdoFood.Name = "rdoFood";
+            this.rdoFood.Size = new System.Drawing.Size(49, 17);
+            this.rdoFood.TabIndex = 0;
+            this.rdoFood.TabStop = true;
+            this.rdoFood.Text = "Food";
+            this.rdoFood.UseVisualStyleBackColor = true;
+            this.rdoFood.CheckedChanged += new System.EventHandler(this.rdoFood_CheckedChanged);
+            // 
+            // rdoWeapons
+            // 
+            this.rdoWeapons.AutoSize = true;
+            this.rdoWeapons.Location = new System.Drawing.Point(118, 19);
+            this.rdoWeapons.Name = "rdoWeapons";
+            this.rdoWeapons.Size = new System.Drawing.Size(71, 17);
+            this.rdoWeapons.TabIndex = 1;
+            this.rdoWeapons.TabStop = true;
+            this.rdoWeapons.Text = "Weapons";
+            this.rdoWeapons.UseVisualStyleBackColor = true;
+            this.rdoWeapons.CheckedChanged += new System.EventHandler(this.rdoWeapons_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "First Aid";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.rdoWeapons);
+            this.groupBox1.Controls.Add(this.rdoFood);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 81);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Categories";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 116);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(205, 125);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "&Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(233, 230);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 34);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(227, 283);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 32);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(329, 49);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(77, 67);
+            this.btnShow.TabIndex = 9;
+            this.btnShow.Text = "&Show Current Supplies";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(451, 452);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Survival Supplies";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdoFood;
+        private System.Windows.Forms.RadioButton rdoWeapons;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ InventoryManagement/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SortedList<string, decimal> foodList = new SortedList<string, decimal>
+            { ["Lunch Meat"] = 7, ["Bread"] = 6, ["Oranges"] = 12, ["Carrots"] = 4, ["Twinkies"] = 40 };
+
+        SortedList<string, decimal> weaponList = new SortedList<string, decimal>
+            { ["Steak Knife"] = 4, ["Shotgun"] = 2, ["Hand Grenade"] = 20, ["Laser Gun"] = 2 };
+
+        SortedList<string, decimal> firstaidList = new SortedList<string, decimal>
+            { ["Bandage"] = 10, ["Rubbing Alchohol"] = 4, ["Hand Sanitizer"] = 2, ["Gauze"] = 5 };
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (KeyValuePair<string, decimal> item in firstaidList)
+            {
+                listBox1.Items.Add(item.Key);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdoFood_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (KeyValuePair<string, decimal> food in foodList)
+            {
+                listBox1.Items.Add(food.Key);
+            }
+        }
+
+        private void rdoWeapons_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (KeyValuePair<string, decimal> weapon in weaponList)
+            {
+                listBox1.Items.Add(weapon.Key);
+            }
+        }
+
+       // private string MasterList(string list, string name)
+       // {
+          //  string newString = "";
+          //  foreach (KeyValuePair<string, decimal> finalList in list)
+           // {
+                //newString += finalList.Key + "\t" + finalList.Value + "\n";
+           // }
+           
+      //  }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            
+            string foodString = "";
+            string aidString = "";
+            string weaponString = "";
+            foreach (KeyValuePair<string, decimal> item in foodList)
+            {
+                foodString +=item.Key + "\t" + item.Value + "\n";
+            }
+            foreach (KeyValuePair<string, decimal> item in firstaidList)
+            {
+                aidString += item.Key + "\t" + item.Value + "\n";
+            }
+            foreach (KeyValuePair<string, decimal> item in weaponList)
+            {
+                weaponString +=item.Key + "\t" + item.Value + "\n";
+            }
+            string bigList = "Food:\n" + foodString + "\n"+"First Aid:\n"+ aidString + "\n"+ "Weapons:\n" + weaponString;
+            MessageBox.Show(bigList,"Master List");
+        }
+    }
+}
