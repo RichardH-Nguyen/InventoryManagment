11d47180af8de3d3c55384be3ffeb57ebfc1ffa8
su7163if <su7163if@T301010-1625344>
2016-11-01 18:28:58 -0500

    New Stuff! Final!

--- InventoryManagement/Form1.Designer.cs
+++ InventoryManagement/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.rdoFood = new System.Windows.Forms.RadioButton();
             this.rdoWeapons = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
@@ -41,6 +42,7 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnItemAdd = new System.Windows.Forms.Button();
             this.txtAddItem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.gboCategories.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
             this.SuspendLayout();
@@ -60,7 +62,7 @@
             // rdoWeapons
             // 
             this.rdoWeapons.AutoSize = true;
-            this.rdoWeapons.Location = new System.Drawing.Point(118, 19);
+            this.rdoWeapons.Location = new System.Drawing.Point(192, 19);
             this.rdoWeapons.Name = "rdoWeapons";
             this.rdoWeapons.Size = new System.Drawing.Size(71, 17);
             this.rdoWeapons.TabIndex = 1;
@@ -72,7 +74,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(12, 49);
+            this.radioButton3.Location = new System.Drawing.Point(94, 19);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(62, 17);
             this.radioButton3.TabIndex = 2;
@@ -83,105 +85,140 @@
             // 
             // gboCategories
             // 
+            this.gboCategories.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.gboCategories.Controls.Add(this.radioButton3);
             this.gboCategories.Controls.Add(this.rdoWeapons);
             this.gboCategories.Controls.Add(this.rdoFood);
-            this.gboCategories.Location = new System.Drawing.Point(11, 12);
+            this.gboCategories.ForeColor = System.Drawing.SystemColors.Control;
+            this.gboCategories.Location = new System.Drawing.Point(11, 27);
             this.gboCategories.Name = "gboCategories";
-            this.gboCategories.Size = new System.Drawing.Size(250, 81);
+            this.gboCategories.Size = new System.Drawing.Size(272, 46);
             this.gboCategories.TabIndex = 3;
             this.gboCategories.TabStop = false;
             this.gboCategories.Text = "Categories";
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 116);
+            this.listBox1.Location = new System.Drawing.Point(163, 144);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // nudAmount
             // 
-            this.nudAmount.Location = new System.Drawing.Point(218, 125);
+            this.nudAmount.BackColor = System.Drawing.SystemColors.MenuText;
+            this.nudAmount.ForeColor = System.Drawing.SystemColors.Window;
+            this.nudAmount.Location = new System.Drawing.Point(34, 144);
             this.nudAmount.Name = "nudAmount";
             this.nudAmount.Size = new System.Drawing.Size(56, 20);
             this.nudAmount.TabIndex = 5;
+            this.nudAmount.ValueChanged += new System.EventHandler(this.nudAmount_ValueChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(157, 151);
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.ForeColor = System.Drawing.Color.Ivory;
+            this.button1.Location = new System.Drawing.Point(15, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "&Take";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(31, 233);
+            this.btnClear.BackColor = System.Drawing.Color.Maroon;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClear.Location = new System.Drawing.Point(85, 259);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(100, 34);
             this.btnClear.TabIndex = 7;
             this.btnClear.Text = "&Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnExit
             // 
+            this.btnExit.BackColor = System.Drawing.Color.Maroon;
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExit.Location = new System.Drawing.Point(339, 408);
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnExit.Location = new System.Drawing.Point(226, 259);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(100, 32);
             this.btnExit.TabIndex = 8;
             this.btnExit.Text = "E&xit";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnShow
             // 
-            this.btnShow.Location = new System.Drawing.Point(74, 352);
+            this.btnShow.BackColor = System.Drawing.Color.Green;
+            this.btnShow.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnShow.Location = new System.Drawing.Point(328, 11);
             this.btnShow.Name = "btnShow";
             this.btnShow.Size = new System.Drawing.Size(77, 67);
             this.btnShow.TabIndex = 9;
             this.btnShow.Text = "&Show Current Supplies";
-            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.UseVisualStyleBackColor = false;
             this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(258, 151);
+            this.btnAdd.BackColor = System.Drawing.Color.Maroon;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAdd.Location = new System.Drawing.Point(15, 184);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "&Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnItemAdd
             // 
-            this.btnItemAdd.Location = new System.Drawing.Point(300, 269);
+            this.btnItemAdd.BackColor = System.Drawing.Color.Green;
+            this.btnItemAdd.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnItemAdd.Location = new System.Drawing.Point(305, 213);
             this.btnItemAdd.Name = "btnItemAdd";
             this.btnItemAdd.Size = new System.Drawing.Size(94, 23);
             this.btnItemAdd.TabIndex = 11;
-            this.btnItemAdd.Text = "Add to Catagory";
-            this.btnItemAdd.UseVisualStyleBackColor = true;
+            this.btnItemAdd.Text = "Add to Category";
+            this.btnItemAdd.UseVisualStyleBackColor = false;
             this.btnItemAdd.Click += new System.EventHandler(this.btnItemAdd_Click);
             // 
             // txtAddItem
             // 
-            this.txtAddItem.Location = new System.Drawing.Point(174, 272);
+            this.txtAddItem.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtAddItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtAddItem.Location = new System.Drawing.Point(305, 187);
             this.txtAddItem.Name = "txtAddItem";
             this.txtAddItem.Size = new System.Drawing.Size(100, 20);
             this.txtAddItem.TabIndex = 12;
+            this.txtAddItem.TextChanged += new System.EventHandler(this.txtAddItem_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Inventory Management";
             // 
             // Form1
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(451, 452);
+            this.ClientSize = new System.Drawing.Size(417, 308);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAddItem);
             this.Controls.Add(this.btnItemAdd);
             this.Controls.Add(this.btnAdd);
@@ -193,7 +230,7 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.gboCategories);
             this.Name = "Form1";
-            this.Text = "Survival Supplies";
+            this.Text = "Zombie Apocalypse Survival Supplies";
             this.gboCategories.ResumeLayout(false);
             this.gboCategories.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
@@ -217,6 +254,7 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnItemAdd;
         private System.Windows.Forms.TextBox txtAddItem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- InventoryManagement/Form1.cs
+++ InventoryManagement/Form1.cs
@@ -16,7 +16,7 @@ namespace InventoryManagement
         {
             InitializeComponent();
         }
-
+        //Initial lists.
         SortedList<string, decimal> foodList = new SortedList<string, decimal>
             { ["Lunch Meat"] = 7, ["Bread"] = 6, ["Oranges"] = 12, ["Carrots"] = 4, ["Twinkies"] = 40 };
 
@@ -26,39 +26,40 @@ namespace InventoryManagement
         SortedList<string, decimal> firstaidList = new SortedList<string, decimal>
             { ["Bandage"] = 10, ["Rubbing Alchohol"] = 4, ["Hand Sanitizer"] = 2, ["Gauze"] = 5 };
 
+        //Initializing strings for our master list/show button
         string aidString = "";
         string foodString = "";
         string weaponString = "";
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateList(firstaidList);
+            UpdateList(firstaidList); //Updates list box when selected.
         }
         private void UpdateList(SortedList<string, decimal> list)
         {
-            listBox1.Items.Clear();
+            listBox1.Items.Clear();     //Clears Listbox
             foreach (KeyValuePair<string, decimal> item in list)
             {
-                listBox1.Items.Add(item.Key);
+                listBox1.Items.Add(item.Key); //Populates listbox with the Keys of a given list.
             }
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //Take button
         {
-            string listItem = listBox1.GetItemText(listBox1.SelectedItem);
-            decimal value = nudAmount.Value;
-            subtract(value, listItem, foodList);
-            subtract(value, listItem, weaponList);
-            subtract(value, listItem, firstaidList);
-            nudAmount.Value = 0;
+            string listItem = listBox1.GetItemText(listBox1.SelectedItem);  //Identifies what is selected in the listbox and converts the name to a string.
+            //Subtracts the count using the subtract method.
+            subtract(nudAmount.Value, listItem, foodList); //Uses subtract method with the given values.
+            subtract(nudAmount.Value, listItem, weaponList);
+            subtract(nudAmount.Value, listItem, firstaidList);
+            nudAmount.Value = 0; //Resets the NumericUpDown button to zero.
         }
 
         private void rdoFood_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateList(foodList);
+            UpdateList(foodList);  //Updates list box when selected.
         }
 
         private void rdoWeapons_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateList(weaponList);
+            UpdateList(weaponList); //Updates list box when selected.
         }
 
         private string MasterList(SortedList<string,decimal> list, string name)
@@ -66,25 +67,24 @@ namespace InventoryManagement
             name = "";
             foreach (KeyValuePair<string, decimal> item in list)
             {
-                name += item.Key + "\t" + item.Value + "\n";
+                name += item.Key + "\t" + item.Value + "\n"; //Gets sorted lists Keys and Values and puts them into a string.
             }
             return name;
         }
 
-        private void btnShow_Click(object sender, EventArgs e)
+        private void btnShow_Click(object sender, EventArgs e) // show entire inventory
         {
-           foodString = MasterList(foodList, foodString);
-           aidString =  MasterList(firstaidList, aidString);
-           weaponString =  MasterList(weaponList, weaponString);
-            string bigList = "Food:\n" + foodString + "\n"+"First Aid:\n"+ aidString + "\n"+ "Weapons:\n" + weaponString;
-            MessageBox.Show(bigList,"Master List");
+           foodString = MasterList(foodList, foodString); //Uses MasterList method and puts result into a string.
+           aidString =  MasterList(firstaidList, aidString); //Uses MasterList method and puts result into a string.
+            weaponString =  MasterList(weaponList, weaponString); //Uses MasterList method and puts result into a string.
+            string bigList = "Food:\n\n" + foodString + "\n"+"First Aid:\n\n"+ aidString + "\n"+ "Weapons:\n\n" + weaponString; //Puts everything into a neat, bigger list.
+            MessageBox.Show(bigList,"Master List"); //Displays final list.
         }
-        private bool subtract(decimal value,string item, SortedList<string, decimal> list)
+        private bool subtract(decimal value,string item, SortedList<string, decimal> list) // subtract quantity from inventory
         {
-            if (list.ContainsKey(item))
+            if (list.ContainsKey(item)) //Checks if the item selected in listbox is actually in it.
             {
-                int itemIndex = list.IndexOfKey(item);
-                list[item] -= value;
+                list[item] -= value; //Subtracts the value of the Key item.
                 return true;
             }
             else
@@ -92,55 +92,102 @@ namespace InventoryManagement
                 
         }
 
-        private bool AddValue(decimal value, string item, SortedList<string, decimal> list)
+        private bool AddValue(decimal value, string item, SortedList<string, decimal> list) // add quantity to inventory
         {
-            if (list.ContainsKey(item))
+            if (list.ContainsKey(item))   //Checks if the item selected in listbox is actually in the list.
             {
-                int itemIndex = list.IndexOfKey(item);
-                list[item] += value;
+                int itemIndex = list.IndexOfKey(item); 
+                list[item] += value; //Adds the value of the Key item.
                 return true;
             }
             else
                 return false;
         }
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e) 
         {
             string listItem = listBox1.GetItemText(listBox1.SelectedItem);
-            decimal value = nudAmount.Value;
-            AddValue(value, listItem, foodList);
-            AddValue(value, listItem, weaponList);
-            AddValue(value, listItem, firstaidList);
+            //Uses AddValue method with given values.
+            AddValue(nudAmount.Value, listItem, foodList);
+            AddValue(nudAmount.Value, listItem, weaponList);
+            AddValue(nudAmount.Value, listItem, firstaidList);
             nudAmount.Value = 0;
         }
 
-        private void btnItemAdd_Click(object sender, EventArgs e)
+        private void btnItemAdd_Click(object sender, EventArgs e) // add item to list
         {
-            string message = "Are you sure you want to add this item to the selected catagory?";
-            DialogResult button = MessageBox.Show(message, "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (button == DialogResult.Yes)
+            if (txtAddItem.Text == "") //Checks for empty string and shows the message box if there's no input.
             {
-                if (rdoFood.Checked)
+                MessageBox.Show("Please enter an item to add.");
+                txtAddItem.Focus();
+            }
+            else //Otherwise...
+            {
+                string message = "Are you sure you want to add this item to the selected category?"; //Confirms whether you are sure you want to add item. 
+                DialogResult button = MessageBox.Show(message, "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (button == DialogResult.Yes)
                 {
-                    foodList.Add(txtAddItem.Text, 0);
-                    UpdateList(foodList);
-                }
-                else if (rdoWeapons.Checked)
-                {
-                    weaponList.Add(txtAddItem.Text, 0);
-                    UpdateList(weaponList);
-                }
-                else if (radioButton3.Checked)
-                {
-                    firstaidList.Add(txtAddItem.Text, 0);
-                    UpdateList(firstaidList);
+                    if (rdoFood.Checked) //Makes sure which radio button is checked.
+                    {
+                        foodList.Add(txtAddItem.Text, 0); // Then adds item to the appropriate list.
+                        UpdateList(foodList);
+                    }
+                    else if (rdoWeapons.Checked)
+                    {
+                        weaponList.Add(txtAddItem.Text, 0);
+                        UpdateList(weaponList);
+                    }
+                    else if (radioButton3.Checked)
+                    {
+                        firstaidList.Add(txtAddItem.Text, 0);
+                        UpdateList(firstaidList);
+                    }
                 }
             }
-            
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void txtAddItem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudAmount_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        { //Clears selected item from list.
+            string message = "Are you sure you want to remove this item from the selected category?";   //Confirms whether are you sure you want to remove item.
+            DialogResult button = MessageBox.Show(message, "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (button == DialogResult.Yes)  //If yes is selected...
+            {
+                string listItem = listBox1.GetItemText(listBox1.SelectedItem); //Gets Item and puts it into a string.
+                if (rdoFood.Checked)
+                {
+                    foodList.Remove(listItem);   //Removes item.
+                    UpdateList(foodList); //Updates listbox immediately.
+                }
+                else if (rdoWeapons.Checked)
+                {
+                    weaponList.Remove(listItem);
+                    UpdateList(weaponList);
+                }
+                else if (radioButton3.Checked)
+                {
+                    firstaidList.Remove(listItem);
+                    UpdateList(firstaidList);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
